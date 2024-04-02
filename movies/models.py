1d0a55f61cd1bab265519b6101f7aa1bada22e88
Akshola00 <akinniyishola07@gmail.com>
2024-04-02 21:35:42 +0100

    new file:   firstdrfproj/__pycache__/__init__.cpython-311.pyc 	new file:   firstdrfproj/__pycache__/settings.cpython-311.pyc 	new file:   firstdrfproj/__pycache__/urls.cpython-311.pyc 	new file:   firstdrfproj/__pycache__/wsgi.cpython-311.pyc 	modified:   firstdrfproj/settings.py 	modified:   firstdrfproj/urls.py 	new file:   movies/__pycache__/__init__.cpython-311.pyc 	new file:   movies/__pycache__/admin.cpython-311.pyc 	modified:   movies/__pycache__/models.cpython-311.pyc 	new file:   movies/__pycache__/serializers.cpython-311.pyc 	new file:   movies/__pycache__/views.cpython-311.pyc 	modified:   movies/admin.py 	new file:   movies/migrations/0001_initial.py 	new file:   movies/migrations/0002_moviedata_typ.py 	new file:   movies/migrations/__pycache__/0002_moviedata_typ.cpython-311.pyc 	modified:   movies/models.py 	new file:   movies/serializers.py

--- movies/models.py
+++ movies/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 class Moviedata(models.Model):
     name= models.CharField(max_length=200)
+    typ = models.CharField(max_length=100, default='action')
     duration= models.FloatField()
     rating = models.FloatField()
+
+    def __str__(self):
+        return self.name
